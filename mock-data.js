705b1c1dfc5daa2e70899a829da6e55125a0671d
QUAN Li <quanccs@163.com>
2019-05-23 14:15:00 +0800

    init

---
+++ mock-data.js
@@ -0,0 +1,357 @@
+let mockData = function () {
+    return {
+        lifecycle: {
+            code: '000000',
+            data: [
+                {
+                    periodGroupRecordList: [
+                        [
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点3',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点4',
+                                                    currentRunType: 2,
+                                                    linkStatus: 1
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 2,
+                                                    linkStatus: 1
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 1
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 1
+                            },
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点3',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点4',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 1
+                            }
+                        ]
+                    ],
+                    workflowGroupName: '测试工作流组',
+                    periodGroupRecordNames: ['测试工作流组']
+                },
+
+                {
+                    periodGroupRecordList: [
+                        [
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点3',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点4',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 1
+                            },
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点3',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点4',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 1
+                            },
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点3',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点4',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 2,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 1
+                            }
+                        ]
+                    ],
+                    workflowGroupName: '测试工作流组',
+                    periodGroupRecordNames: ['测试工作流组']
+                },
+
+                {
+                    periodGroupRecordList: [
+                        [
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 2
+                            },
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 2
+                            },
+                            {
+                                periodDetails: [
+                                    {
+                                        lifeCyclePartVO: {
+                                            links: [
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点1',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点3',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点4',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                },
+                                                {
+                                                    linkName: '节点5',
+                                                    currentRunType: 1,
+                                                    linkStatus: 0
+                                                }
+                                            ],
+                                            workFlowDO: {
+                                                flowName: '测试工作流'
+                                            }
+                                        }
+                                    }
+                                ],
+                                runType: 2
+                            }
+                        ],
+                    ],
+                    workflowGroupName: '测试工作流组3',
+                    periodGroupRecordNames: ['测试工作流组3']
+                }
+            ]
+        }
+    }
+};
